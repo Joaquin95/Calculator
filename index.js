cc87cde87e5b41661ea9fd1ebd79f381a8f3dae8
Joaquin Morales <mintinvestments95@gmail.com>
2025-02-11 17:00:48 -0600

    finished calculator

---
+++ index.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+ display.value = "";
+}
+
+function calculate(){
+    try {
+        display.value = eval(display.value);
+    } catch (e){
+        display.value = "Error";
+    }
+}
+
+window.appendToDisplay = appendToDisplay;
+window.clearDisplay = clearDisplay;
+window.calculate = calculate;
